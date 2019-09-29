c94a4b38f487f9b995702752c9c942de66e6d3ee
TanTan Wang <tantan.wang@yale.edu>
2019-09-29 00:46:14 -0400

    processing started

--- fartmachine-arduino/fartmachine-arduino.ino
+++ fartmachine-arduino/fartmachine-arduino.ino
@@ -1,3 +1,12 @@
+
+/*Things to do:
+ * 
+ * Turn on an LED when the device is on. When the button is pressed
+ * have the LED turn off. The switch changes the color of the LED
+ * based on the fart track.
+ * 
+ */
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
